0ffaf1670c09a49f6c1e8fa654fa235c4c6ad2ce
luizantoniona <luizantonio.na@hotmail.com>
2023-08-30 20:41:23 -0300

    feat: criar sql para inserir informações de sumario

---
+++ notebook/database/sql/insert_summary_info.sql
@@ -0,0 +1,21 @@
+INSERT INTO summary_info
+(
+    record_name,
+    file_name,
+    start_time,
+    end_time,
+    nr_seizures,
+    start_seizure,
+    end_seizure,
+    nr_channels 
+)
+VALUES (
+    %s,
+    %s,
+    %s,
+    %s,
+    %s,
+    %s,
+    %s,
+    %s
+);
