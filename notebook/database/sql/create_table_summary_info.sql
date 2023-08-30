dc15b6e867d5f0f17aaffac84f2bde90e6be395f
luizantoniona <luizantonio.na@hotmail.com>
2023-08-30 14:09:04 -0300

    feat: criando sql para criação da tabela onde será armazenado as informações dos sumários

---
+++ notebook/database/sql/create_table_summary_info.sql
@@ -0,0 +1,11 @@
+CREATE TABLE summary_info
+(
+    record_name VARCHAR(20),
+    file_name VARCHAR(20),
+    start_time TIME,
+    end_time TIME,
+    nr_seizures INTEGER,
+    start_seizure INTEGER,
+    end_seizure INTEGER,
+    nr_channels INTEGER 
+);
