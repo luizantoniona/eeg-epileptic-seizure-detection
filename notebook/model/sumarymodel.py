88659433b6f09118ea0f7845d39af7bf9219192b
luizantoniona <luizantonio.na@hotmail.com>
2023-08-31 22:41:14 -0300

    feat: criar classe para armazenar as informações de cada exame, será utilizado posteriormente

---
+++ notebook/model/sumarymodel.py
@@ -0,0 +1,24 @@
+class SummaryModel:
+    record_name = ""
+    file_name = ""
+    start_time = ""
+    end_time = ""
+    nr_seizures = 0
+    start_seizure = ""
+    end_seizure = ""
+    nr_channels = 0
+    ds_channels = ""
+
+    def __init__(self, record_name, file_name, start_time, end_time, nr_seizures, start_seizure, end_seizure, nr_channels, ds_channels):
+        self.record_name = record_name
+        self.file_name = file_name
+        self.start_time = start_time
+        self.end_time = end_time
+        self.nr_seizures = nr_seizures
+        self.start_seizure = start_seizure
+        self.end_seizure = end_seizure
+        self.nr_channels = nr_channels
+        self.ds_channels = ds_channels
+
+    def __str__(self):
+        return f"{self.record_name}:({self.file_name})"
