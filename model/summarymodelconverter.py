ab2aab959c41ffc18a2399f5067b57c4caf36312
luizantoniona <luizantonio.na@hotmail.com>
2023-09-28 23:52:16 -0300

    fix: corrigir lista caso n possua evento

--- model/summarymodelconverter.py
+++ model/summarymodelconverter.py
@@ -1,14 +1,24 @@
 import model.summarymodel as summary
 
 def model_from_tuple(summary_tupple):
+
+    summary_start_times = []
+    summary_end_times = []
+
+    if summary_tupple['start_seizure'] != '':
+        summary_start_times = [int(x) for x in str(summary_tupple['start_seizure']).split(',')]
+
+    if summary_tupple['end_seizure'] != '':
+        summary_end_times = [int(x) for x in str(summary_tupple['end_seizure']).split(',')]
+
     model = summary.SummaryModel (
         summary_tupple['record_name'],
         summary_tupple['file_name'],
         summary_tupple['start_time'],
         summary_tupple['end_time'],
         summary_tupple['nr_seizures'],
-        [int(x) for x in str(summary_tupple['start_seizure']).split(',')],
-        [int(x) for x in str(summary_tupple['end_seizure']).split(',')],
+        summary_start_times,
+        summary_end_times,
         summary_tupple['nr_channels'],
         str(summary_tupple['ds_channels']).split(',')
     )
