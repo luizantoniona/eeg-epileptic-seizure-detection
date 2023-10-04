c7ecb0a9822082c5c8025ba9a65c7f08634c56d8
luizantoniona <luizantonio.na@hotmail.com>
2023-10-04 13:18:05 -0300

    feat: criando loader, retorna os exames em cada um dos tipos de domínio

---
+++ datautils/dataloader.py
@@ -0,0 +1,27 @@
+import database.databaseutils as db
+import model.summarymodelconverter as converter
+import reader.reader as reader
+
+def load_summaries():
+    all_summaries = []
+
+    db.connect()
+    db_objects = db.all_summary()
+
+    for db_object in db_objects:
+        all_summaries.append(converter.model_from_tuple( db_object ))
+
+    print(len(all_summaries))
+
+    return all_summaries
+
+def load_mne_objects():
+
+    all_mne_object = []
+
+    for summary in load_summaries():
+        all_mne_object.append(reader.mne_edf(summary))
+
+    print(len(all_mne_object))
+
+    return all_mne_object
