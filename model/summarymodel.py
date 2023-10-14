1875d18b2c8308bcc2520f524a4b8e9d1cf09e5f
Luiz Antonio Nicolau Anghinoni <luiz.anghinoni@ciss.com.br>
2023-10-14 15:20:59 -0300

    fix: remover da inicialização do objeto e deixar para método

--- model/summarymodel.py
+++ model/summarymodel.py
@@ -11,7 +11,7 @@ class SummaryModel:
     nr_channels = 0
     ds_channels = []
 
-    def __init__(self, record_name, file_name, start_time, end_time, nr_seizures, start_seizure, end_seizure, nr_channels, ds_channels, rename = False):
+    def __init__(self, record_name, file_name, start_time, end_time, nr_seizures, start_seizure, end_seizure, nr_channels, ds_channels):
         self.record_name = record_name
         self.file_name = file_name
         self.start_time = start_time
@@ -21,26 +21,6 @@ class SummaryModel:
         self.end_seizure = end_seizure
         self.nr_channels = nr_channels
         self.ds_channels = ds_channels
-
-        time_data = reader.mne_edf(self)
-
-        if( rename ):
-            replace_dict = {}
-            drop_list = []
-            for channel_name in time_data.info['ch_names']:
-                name_change = re.findall('\w+',channel_name)[0].title()
-                if name_change in list(replace_dict.values()):
-                    drop_list.append(channel_name)
-                else:
-                    replace_dict[channel_name] = name_change
-
-            time_data.drop_channels(drop_list)
-            time_data.rename_channels(replace_dict)
-            time_data.set_montage('standard_1020')
-            
-        self.time_data = time_data
-        self.psd_data = time_data.copy().compute_psd()
-        self.spec_data = "" #TODO: Calcular Espectro de Potência
 
     def __str__(self):
         return f"{self.record_name}:({self.file_name})"
@@ -63,3 +43,25 @@ class SummaryModel:
             return 0
         else:
             return self.end_seizure[nr_seizure - 1]
+        
+    def compute_mne_objects(self, rename = False):
+
+        time_data = reader.mne_edf(self)
+
+        if( rename ):
+            replace_dict = {}
+            drop_list = []
+            for channel_name in time_data.info['ch_names']:
+                name_change = re.findall('\w+',channel_name)[0].title()
+                if name_change in list(replace_dict.values()):
+                    drop_list.append(channel_name)
+                else:
+                    replace_dict[channel_name] = name_change
+
+            time_data.drop_channels(drop_list)
+            time_data.rename_channels(replace_dict)
+            time_data.set_montage('standard_1020')
+            
+        self.time_data = time_data
+        self.psd_data = time_data.copy().compute_psd()
+        self.spec_data = "" #TODO: Calcular Espectro de Potência
--- model/summarymodelconverter.py
+++ model/summarymodelconverter.py
@@ -1,6 +1,6 @@
 import model.summarymodel as summary
 
-def model_from_tuple(summary_tupple, rename = False):
+def model_from_tuple(summary_tupple):
 
     summary_start_times = []
     summary_end_times = []
@@ -20,8 +20,7 @@ def model_from_tuple(summary_tupple, rename = False):
         summary_start_times,
         summary_end_times,
         summary_tupple['nr_channels'],
-        str(summary_tupple['ds_channels']).split(','),
-        rename
+        str(summary_tupple['ds_channels']).split(',')
     )
 
     return model
