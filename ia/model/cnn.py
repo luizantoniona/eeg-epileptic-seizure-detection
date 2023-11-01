c6b8b0d24546493897d6b752e393b539cdd1aa39
luizantoniona <luizantonio.na@hotmail.com>
2023-10-31 21:18:09 -0300

    ajustando modelo de cnn para receber dados temporais

--- ia/model/cnn.py
+++ ia/model/cnn.py
@@ -1,12 +1,18 @@
 import tensorflow as tf
-
 class CNNModel:
 
-    def __init__(self):
-
+    def __init__(self, temporal_length, num_channels):
         self.model = tf.keras.models.Sequential()
-        self.model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-        self.model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-        self.model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
-        self.model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-        self.model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(tf.keras.layers.Conv1D(32, 3, activation='relu', input_shape=(temporal_length, num_channels)))
+        self.model.add(tf.keras.layers.MaxPooling1D(2))
+        self.model.add(tf.keras.layers.Conv1D(64, 3, activation='relu'))
+        self.model.add(tf.keras.layers.MaxPooling1D(2))
+        self.model.add(tf.keras.layers.Flatten())
+        self.model.add(tf.keras.layers.Dense(128, activation='relu'))
+        self.model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+
+    def compile(self):
+        self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    def fit(self, train_data, train_labels, num_epochs, val_data, val_labels):
+        self.model.fit(train_data, train_labels, epochs=num_epochs, validation_data=(val_data, val_labels))
