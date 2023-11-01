2af9fbaf366626c10e726a3ecba8cd3dc49cbfc2
luizantoniona <luizantonio.na@hotmail.com>
2023-10-31 21:20:29 -0300

    criado método para calcular espectro de potencia e espectograma

--- model/signalmodel.py
+++ model/signalmodel.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 class SignalModel:
     def __init__(self, channels_names, channels_frequencies, channels_buffers, times):
         self.channels_names = channels_names
@@ -7,3 +9,22 @@ class SignalModel:
 
     def __str__(self):
         return f"{self.channels_names}"
+    
+    def psd(self):
+        psds = []
+
+        for buffer in self.channels_buffers:
+            fft_result = np.fft.fft(buffer)
+            espectro_potencia = np.abs(fft_result)**2 / len(buffer)
+            psds.append(espectro_potencia)
+
+        self.channels_psds = psds
+
+    def spc(self):
+        spectograms = []
+        
+        for buffer in self.channels_buffers:
+            f, t, Sxx = plt.specgram(buffer, Fs=self.channels_frequencies)
+            spectograms.append((f, t, Sxx))
+
+        self.channels_spectograms = spectograms
