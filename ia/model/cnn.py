bc1a27f235fa2b155eebcd6b450c177d7193bbab
luizantoniona <luizantonio.na@hotmail.com>
2023-11-15 17:06:50 -0300

    correções no input shape da cnn

--- ia/model/cnn.py
+++ ia/model/cnn.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 class CNNModel:
 
-    def __init__(self, channel_buffers, num_channels):
+    def __init__(self, input_shape):
         self.model = tf.keras.models.Sequential()
-        self.model.add(tf.keras.layers.Conv1D(32, 3, activation='relu', input_shape=(channel_buffers, num_channels)))
-        self.model.add(tf.keras.layers.MaxPooling1D(2))
+        self.model.add(tf.keras.layers.Conv1D(32, 3, activation='relu', input_shape=(input_shape)))
+        self.model.add(tf.keras.layers.MaxPooling1D(3))
         self.model.add(tf.keras.layers.Conv1D(64, 3, activation='relu'))
-        self.model.add(tf.keras.layers.MaxPooling1D(2))
+        self.model.add(tf.keras.layers.MaxPooling1D(3))
         self.model.add(tf.keras.layers.Flatten())
         self.model.add(tf.keras.layers.Dense(128, activation='relu'))
         self.model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
@@ -14,5 +14,5 @@ class CNNModel:
     def compile(self):
         self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    def fit(self, train_data, train_labels, num_epochs, val_data, val_labels):
-        self.model.fit(train_data, train_labels, epochs=num_epochs, validation_data=(val_data, val_labels))
+    def fit(self, train_data, train_labels, num_epochs, batch_size, val_data, val_labels):
+        self.model.fit(train_data, train_labels, epochs=num_epochs, batch_size=batch_size, validation_data=(val_data, val_labels))
