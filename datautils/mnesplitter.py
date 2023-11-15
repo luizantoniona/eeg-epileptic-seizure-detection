00dbef7a0c01559a0085b422871dfabe04b1036d
luizantoniona <luizantonio.na@hotmail.com>
2023-11-15 17:07:52 -0300

    corrigindo splitter de dados temporais

--- datautils/mnesplitter.py
+++ datautils/mnesplitter.py
@@ -1,10 +1,11 @@
 import model.summarymodel as sm
 import datautils.summarysplitter as splitter
+import numpy as np
     
 def time_data_splitter(summaries: list[sm.SummaryModel]):
     X_train, X_val, y_train, y_val = splitter.summaries_data_splitter(summaries)
 
-    X_train_raw = [summary.signal.time_data.get_data() for summary in X_train]
-    X_val_raw = [summary.signal.time_data.get_data() for summary in X_val]
+    X_train_time_data = np.array([summary.signal.get_time_data()[0] for summary in X_train])
+    X_val_time_data = np.array([summary.signal.get_time_data()[0] for summary in X_val])
 
     return X_train_raw, X_val_raw, y_train, y_val
