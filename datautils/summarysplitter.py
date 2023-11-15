cca16a89d6cc650140660adbf282c2723cb07910
luizantoniona <luizantonio.na@hotmail.com>
2023-11-15 15:16:06 -0300

    explicitar retorno do método

--- datautils/summarysplitter.py
+++ datautils/summarysplitter.py
@@ -2,13 +2,13 @@ import model.summarymodel as sm
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def summaries_data_splitter(summaries: list[sm.SummaryModel]):
+def summaries_data_splitter(summaries: list[sm.SummaryModel]) -> (list[sm.SummaryModel], list[sm.SummaryModel], np.array, np.array) :
     X_train, X_val = train_test_split(summaries, test_size=0.2, random_state=42)
 
     y_train = [summary.has_anomaly() for summary in X_train]
     y_val = [summary.has_anomaly() for summary in X_val]
 
-    y_train = np.array(np.multiply( y_train, 1 ))
-    y_val = np.array(np.multiply( y_val, 1 ))
+    y_train = np.array( y_train)
+    y_val = np.array(y_val)
 
     return X_train, X_val, y_train, y_val
