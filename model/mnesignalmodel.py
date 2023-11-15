17067cd0b3fa145a8220682ebf9ad176fae30dda
luizantoniona <luizantonio.na@hotmail.com>
2023-11-15 15:15:39 -0300

    criando métodos para retornar os atributos de forma correta

--- model/mnesignalmodel.py
+++ model/mnesignalmodel.py
@@ -7,8 +7,17 @@ class MNESignalModel:
         self.time_freq_data = []
 
         for buffer in mne_object.get_data():
-            f, t, Sxx = spectrogram(buffer, fs=mne_object.info['sfreq'])
-            self.time_freq_data.append((f, t, Sxx))
+            freq, time, Sxx = spectrogram(buffer, fs=mne_object.info['sfreq'])
+            self.time_freq_data.append((freq, time, Sxx))
+
+    def get_time_data(self):
+        return self.time_data.get_data(return_times=True)
+    
+    def get_freq_data(self):
+        return self.freq_data.get_data(return_freqs=True)
+    
+    def get_time_fre_data(self):
+        return self.time_freq_data
 
     def sampling_freq(self):
         return self.time_data.info['sfreq']
