3340d5707c4ac3ee694183bada136101ab7e1bba
luizantoniona <luizantonio.na@hotmail.com>
2023-11-15 14:20:21 -0300

    documentação da classe

--- model/summarymodel.py
+++ model/summarymodel.py
@@ -12,6 +12,7 @@ class SummaryModel:
     end_seizure = []
     nr_channels = 0
     ds_channels = []
+    signal = None
 
     def __init__(self, record_name, file_name, start_time, end_time, nr_seizures, start_seizure, end_seizure, nr_channels, ds_channels):
         self.record_name = record_name
@@ -28,29 +29,36 @@ class SummaryModel:
         return f"{self.record_name}:({self.file_name})"
     
     def fullpath(self):
+        """Return the full path of the data file."""
         return "./data/" + self.record_name + "/" + self.file_name
     
     def duration(self):
+        """Return the duration of the data in seconds."""
         duration = self.end_time - self.start_time
         return duration.total_seconds()
     
     def start_time_of_seizure(self, nr_seizure = 0):
+        """Return the start time of a specific seizure."""
         if nr_seizure > self.nr_seizures:
             return 0
         else:
             return self.start_seizure[nr_seizure - 1]
         
     def end_time_of_seizure(self, nr_seizure = 0):
+        """Return the end time of a specific seizure."""
         if nr_seizure > self.nr_seizures:
             return 0
         else:
             return self.end_seizure[nr_seizure - 1]
         
     def has_anomaly(self) -> bool:
+        """Check if there are seizures."""
         return self.nr_seizures > 0
         
     def generate_mne(self, rename = False) -> None:
+        """Generate an MNE signal model."""
         self.signal =  mnesignal.MNESignalModel( mnereader.mne_edf(self, rename) )
 
     def generate_signal(self) -> None:
+        """Generate a raw signal model."""
         self.signal = rawsignal.RawSignalModel( *reader.edf(self) )
