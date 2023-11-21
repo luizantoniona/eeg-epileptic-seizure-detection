528c6cc0f265a11f1e18c52026ba8558e39d671c
luizantoniona <luizantonio.na@hotmail.com>
2023-11-20 23:22:36 -0300

    documentação dos métodos

--- datautils/mnesplitter.py
+++ datautils/mnesplitter.py
@@ -3,6 +3,7 @@ import datautils.summarysplitter as splitter
 import numpy as np
     
 def time_data_splitter(summaries: list[sm.SummaryModel]):
+    """Split summaries into training and validation sets based on time-domain data."""
     X_train, X_val, y_train, y_val = splitter.summaries_data_splitter(summaries)
 
     X_train_time_data = np.array([summary.signal.get_time_data()[0] for summary in X_train])
@@ -11,6 +12,7 @@ def time_data_splitter(summaries: list[sm.SummaryModel]):
     return X_train_time_data, X_val_time_data, y_train, y_val
 
 def freq_data_splitter(summaries: list[sm.SummaryModel]):
+    """Split summaries into training and validation sets based on frequency-domain data."""
     X_train, X_val, y_train, y_val = splitter.summaries_data_splitter(summaries)
 
     X_train_freq_data = np.array([summary.signal.get_freq_data()[0] for summary in X_train])
@@ -19,6 +21,7 @@ def freq_data_splitter(summaries: list[sm.SummaryModel]):
     return X_train_freq_data, X_val_freq_data, y_train, y_val
 
 def time_freq_data_splitter(summaries: list[sm.SummaryModel]):
+    """Split summaries into training and validation sets based on time-frequency data."""
     X_train, X_val, y_train, y_val = splitter.summaries_data_splitter(summaries)
 
     X_train_time_freq_data = [summary.signal.get_time_freq_data() for summary in X_train]
