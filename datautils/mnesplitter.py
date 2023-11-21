3d7e268bb06916c6bbdc064c375f2c9e59a3e33a
luizantoniona <luizantonio.na@hotmail.com>
2023-11-20 23:28:42 -0300

    criar métrodo para split de dados segmentados em todos os dominios

--- datautils/mnesplitter.py
+++ datautils/mnesplitter.py
@@ -11,6 +11,15 @@ def time_data_splitter(summaries: list[sm.SummaryModel]):
 
     return X_train_time_data, X_val_time_data, y_train, y_val
 
+def segmented_time_data_splitter(summaries: list[sm.SummaryModel]):
+    """Split segmented summaries into training and validation sets based on time-domain data."""
+    X_train, X_val, y_train, y_val = splitter.segmented_summaries_data_splitter(summaries)
+
+    X_train_segmented_time_data = np.array([summary.signal.get_time_segmented_data() for summary in X_train])
+    X_val_segmented_time_data = np.array([summary.signal.get_time_segmented_data() for summary in X_val])
+
+    return X_train_segmented_time_data, X_val_segmented_time_data, y_train, y_val
+
 def freq_data_splitter(summaries: list[sm.SummaryModel]):
     """Split summaries into training and validation sets based on frequency-domain data."""
     X_train, X_val, y_train, y_val = splitter.summaries_data_splitter(summaries)
@@ -20,6 +29,15 @@ def freq_data_splitter(summaries: list[sm.SummaryModel]):
 
     return X_train_freq_data, X_val_freq_data, y_train, y_val
 
+def segmented_freq_data_splitter(summaries: list[sm.SummaryModel]):
+    """Split segmented summaries into training and validation sets based on frequency-domain data."""
+    X_train, X_val, y_train, y_val = splitter.segmented_summaries_data_splitter(summaries)
+
+    X_train_segmented_freq_data = np.array([summary.signal.get_freq_segmented_data()[0] for summary in X_train])
+    X_val_segmented_freq_data = np.array([summary.signal.get_freq_segmented_data()[0] for summary in X_val])
+
+    return X_train_segmented_freq_data, X_val_segmented_freq_data, y_train, y_val
+
 def time_freq_data_splitter(summaries: list[sm.SummaryModel]):
     """Split summaries into training and validation sets based on time-frequency data."""
     X_train, X_val, y_train, y_val = splitter.summaries_data_splitter(summaries)
@@ -28,3 +46,12 @@ def time_freq_data_splitter(summaries: list[sm.SummaryModel]):
     X_val_time_freq_data = [summary.signal.get_time_freq_data() for summary in X_val]
 
     return X_train_time_freq_data, X_val_time_freq_data, y_train, y_val
+
+def segmented_time_freq_data_splitter(summaries: list[sm.SummaryModel]):
+    """Split segmented summaries into training and validation sets based on time-frequency data."""
+    X_train, X_val, y_train, y_val = splitter.segmented_summaries_data_splitter(summaries)
+
+    X_train_segmented_time_freq_data = [summary.signal.get_time_freq_segmented_data() for summary in X_train]
+    X_val_segmented_time_freq_data = [summary.signal.get_time_freq_segmented_data() for summary in X_val]
+
+    return X_train_segmented_time_freq_data, X_val_segmented_time_freq_data, y_train, y_val
