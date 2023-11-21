8d0e21c136db3e5c705301030de4b6afa1264ec6
luizantoniona <luizantonio.na@hotmail.com>
2023-11-20 22:14:21 -0300

    indentação

--- datautils/summarysplitter.py
+++ datautils/summarysplitter.py
@@ -2,7 +2,7 @@ import model.summarymodel as sm
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def summaries_data_splitter(summaries: list[sm.SummaryModel]) -> (list[sm.SummaryModel], list[sm.SummaryModel], np.array, np.array) :
+def summaries_data_splitter(summaries: list[sm.SummaryModel]) -> (list[sm.SummaryModel], list[sm.SummaryModel], np.array, np.array):
     X_train, X_val = train_test_split(summaries, test_size=0.2, random_state=42)
 
     y_train = [summary.has_anomaly() for summary in X_train]
