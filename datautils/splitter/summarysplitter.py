16358a642200b956c1c8f176bb6647c2833b030c
luizantoniona <luizantonio.na@hotmail.com>
2023-11-23 13:16:41 -0300

    documentação do módulo

--- datautils/splitter/summarysplitter.py
+++ datautils/splitter/summarysplitter.py
@@ -1,8 +1,17 @@
+"""
+Module: datasplitter
+
+This module provides functions to split summary data into training and validation sets.
+"""
+
 import model.summarymodel as sm
 import numpy as np
 from sklearn.model_selection import train_test_split
 
 def summaries_data_splitter(summaries: list[sm.SummaryModel]) -> (list[sm.SummaryModel], list[sm.SummaryModel], np.array, np.array):
+    """
+    Split summary data into training and validation sets for model training.
+    """
     X_train, X_val = train_test_split(summaries, test_size=0.2, random_state=42)
 
     y_train = [summary.has_anomaly() for summary in X_train]
@@ -14,6 +23,9 @@ def summaries_data_splitter(summaries: list[sm.SummaryModel]) -> (list[sm.Summar
     return X_train, X_val, y_train, y_val
 
 def segmented_summaries_data_splitter(summaries: list[sm.SummaryModel]) -> (list[sm.SummaryModel], list[sm.SummaryModel], np.array, np.array):
+    """
+    Split segmented summary data into training and validation sets for model training.
+    """
     X_train, X_val = train_test_split(summaries, test_size=0.2, random_state=42)
 
     y_train = np.concatenate([summary.anomalies_by_time_window() for summary in X_train])
