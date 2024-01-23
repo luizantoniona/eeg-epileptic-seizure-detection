ef2aa4152f46127c9026ea43455236d385c9a3d9
luizantoniona <luizantonio.na@hotmail.com>
2024-01-23 20:49:31 -0300

    create a new class for common methods of CNN, and make them based on new class

--- ia/model/cnn.py
+++ ia/model/cnn.py
@@ -5,12 +5,29 @@ This module provides CNN models for each data domain
 """
 
 import tensorflow as tf
-class TimeCNN:
+
+class BaseCNN:
+    """
+    CNN Basic Model for common methods
+    """
+    def __init__(self):
+        self.model = tf.keras.models.Sequential()
+
+    def compile(self):
+        self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    def fit(self, train_data, train_labels, num_epochs, batch_size, val_data, val_labels):
+        return self.model.fit(train_data, train_labels, epochs=num_epochs, batch_size=batch_size, validation_data=(val_data, val_labels))
+
+    def summary(self):
+        self.model.summary()
+
+class TimeCNN( BaseCNN ):
     """
     CNN Model for time data training
     """
     def __init__(self, input_shape):
-        self.model = tf.keras.models.Sequential()
+        super().__init__()
         self.model.add(tf.keras.layers.Conv1D(32, 3, activation='relu', input_shape=(input_shape)))
         self.model.add(tf.keras.layers.MaxPooling1D(3))
         self.model.add(tf.keras.layers.Conv1D(64, 3, activation='relu'))
@@ -19,48 +36,30 @@ class TimeCNN:
         self.model.add(tf.keras.layers.Dense(128, activation='relu'))
         self.model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
-    def compile(self):
-        self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-    def fit(self, train_data, train_labels, num_epochs, batch_size, val_data, val_labels):
-        self.model.fit(train_data, train_labels, epochs=num_epochs, batch_size=batch_size, validation_data=(val_data, val_labels))
-
-class FrequencyCNN:
+class FrequencyCNN( BaseCNN ):
     """
     CNN Model for frequency data training
     """
     def __init__(self, input_shape):
-        self.model = tf.keras.models.Sequential()
-        self.model.add(tf.keras.layers.Conv1D(32, 3, activation='relu', input_shape=(input_shape)))
-        self.model.add(tf.keras.layers.MaxPooling1D(3))
-        self.model.add(tf.keras.layers.Conv1D(64, 3, activation='relu'))
-        self.model.add(tf.keras.layers.MaxPooling1D(3))
+        super().__init__()
+        self.model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(input_shape)))
+        self.model.add(tf.keras.layers.MaxPooling2D((3, 3)))
+        self.model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(tf.keras.layers.MaxPooling2D((3, 3)))
         self.model.add(tf.keras.layers.Flatten())
         self.model.add(tf.keras.layers.Dense(128, activation='relu'))
         self.model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
-    def compile(self):
-        self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-    def fit(self, train_data, train_labels, num_epochs, batch_size, val_data, val_labels):
-        self.model.fit(train_data, train_labels, epochs=num_epochs, batch_size=batch_size, validation_data=(val_data, val_labels))
-
-class TimeFrequencyCNN:
+class TimeFrequencyCNN( BaseCNN ):
     """
     CNN Model for time-frequency data training
     """
     def __init__(self, input_shape):
-        self.model = tf.keras.models.Sequential()
-        self.model.add(tf.keras.layers.Conv1D(32, 3, activation='relu', input_shape=(input_shape)))
-        self.model.add(tf.keras.layers.MaxPooling1D(3))
-        self.model.add(tf.keras.layers.Conv1D(64, 3, activation='relu'))
-        self.model.add(tf.keras.layers.MaxPooling1D(3))
+        super().__init__()
+        self.model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(input_shape)))
+        self.model.add(tf.keras.layers.MaxPooling2D((3, 3)))
+        self.model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(tf.keras.layers.MaxPooling2D((3, 3)))
         self.model.add(tf.keras.layers.Flatten())
         self.model.add(tf.keras.layers.Dense(128, activation='relu'))
         self.model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
-
-    def compile(self):
-        self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-    def fit(self, train_data, train_labels, num_epochs, batch_size, val_data, val_labels):
-        self.model.fit(train_data, train_labels, epochs=num_epochs, batch_size=batch_size, validation_data=(val_data, val_labels))
