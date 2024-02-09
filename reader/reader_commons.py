ee427fe18c462fb33a9efde97f435fc304a8a4bd
luizantoniona <luizantonio.na@hotmail.com>
2024-02-09 00:58:47 -0300

    doc: add function description

---
+++ reader/reader_commons.py
@@ -0,0 +1,29 @@
+"""
+Module: reader_commons
+"""
+
+def selected_channels():
+  """
+    Returns a list of selected EEG (Electroencephalography) channel names commonly used in neurophysiological studies.
+    A list of selected EEG channel names. Each channel name represents a specific electrode placement on the scalp.
+    """
+  return [
+    'P8-O2',
+    'CZ-PZ',
+    'T8-P8',
+    'T7-P7',
+    'FZ-CZ',
+    'C3-P3',
+    'P4-O2',
+    'C4-P4',
+    'FP1-F7',
+    'F3-C3',
+    'F4-C4',
+    'P7-O1',
+    'FP2-F8',
+    'F7-T7',
+    'FP2-F4',
+    'FP1-F3',
+    'P3-O1',
+    'F8-T8'
+  ]
