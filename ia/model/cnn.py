dd439547de530dc0a43ee5da112fdd41b24199e8
luizantoniona <luizantonio.na@hotmail.com>
2024-02-20 20:09:17 -0300

    new models for frequency and time-frequency

--- ia/model/cnn.py
+++ ia/model/cnn.py
@@ -42,12 +42,16 @@ class FrequencyCNN( BaseCNN ):
     """
     def __init__(self, input_shape):
         super().__init__()
-        self.model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(input_shape)))
-        self.model.add(tf.keras.layers.MaxPooling2D((3, 3)))
-        self.model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
-        self.model.add(tf.keras.layers.MaxPooling2D((3, 3)))
+        self.model.add(tf.keras.layers.Conv1D(32, 3, activation='relu', input_shape=input_shape))
+        self.model.add(tf.keras.layers.MaxPooling1D(3))
+        self.model.add(tf.keras.layers.BatchNormalization())
+        self.model.add(tf.keras.layers.Conv1D(64, 3, activation='relu'))
+        self.model.add(tf.keras.layers.MaxPooling1D(3))
+        self.model.add(tf.keras.layers.BatchNormalization())
+        self.model.add(tf.keras.layers.GlobalMaxPooling1D())
         self.model.add(tf.keras.layers.Flatten())
         self.model.add(tf.keras.layers.Dense(128, activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.5))
         self.model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
 class TimeFrequencyCNN( BaseCNN ):
@@ -56,10 +60,14 @@ class TimeFrequencyCNN( BaseCNN ):
     """
     def __init__(self, input_shape):
         super().__init__()
-        self.model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(input_shape)))
-        self.model.add(tf.keras.layers.MaxPooling2D((3, 3)))
-        self.model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
-        self.model.add(tf.keras.layers.MaxPooling2D((3, 3)))
+        self.model.add(tf.keras.layers.Conv2D(64, (2,2), activation='relu', input_shape=input_shape))
+        self.model.add(tf.keras.layers.MaxPooling2D((2,2)))
+        self.model.add(tf.keras.layers.BatchNormalization())
+        self.model.add(tf.keras.layers.Conv2D(128, (2,2), activation='relu'))
+        self.model.add(tf.keras.layers.MaxPooling2D((2,2)))
+        self.model.add(tf.keras.layers.BatchNormalization())
+        self.model.add(tf.keras.layers.GlobalMaxPooling2D())
         self.model.add(tf.keras.layers.Flatten())
-        self.model.add(tf.keras.layers.Dense(128, activation='relu'))
+        self.model.add(tf.keras.layers.Dense(256, activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.5))
         self.model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
