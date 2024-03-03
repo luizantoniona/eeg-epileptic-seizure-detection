ead42a511fc1ae3df525c4a79f194744b5866e5c
luizantoniona <luizantonio.na@hotmail.com>
2024-03-03 19:33:47 -0300

    fix: return values instead of printing && general adjustments

--- ia/metrics/metrics_generator.py
+++ ia/metrics/metrics_generator.py
@@ -1,83 +1,89 @@
 """
 Module: metrics_generator
 
-Provides class to calculate metrics
+Provides class to calculate metrics from test and prediction
 """
 
 class Metrics:
    """
    """
-   def __init__(self, y_test, predictions):
+   def __init__(self, test, predictions):
       self.TruePositives = 0
       self.TrueNegatives = 0
       self.FalsePositives = 0
       self.FalseNegatives = 0
-      self.Total = len(y_test)
+      self.Total = len(test)
 
       for i in range(self.Total):
-         if y_test[i] == True and predictions[i] == True:
+         if test[i] == True and predictions[i] == True:
             self.TruePositives += 1
-         elif y_test[i] == False and predictions[i] == False:
+         elif test[i] == False and predictions[i] == False:
             self.TrueNegatives += 1
-         elif y_test[i] == True and predictions[i] == False:
+         elif test[i] == True and predictions[i] == False:
             self.FalsePositives += 1
-         elif y_test[i] == False and predictions[i] == True:
+         elif test[i] == False and predictions[i] == True:
             self.FalseNegatives += 1
 
-   def accuracy(self):
+      print('True Positives:', self.TruePositives)
+      print('True Negatives:', self.TrueNegatives)
+      print('False Positives:', self.FalsePositives)
+      print('False Negatives:', self.FalseNegatives)
+      print('Total Samples:', self.Total)
+
+   def accuracy(self) -> float:
       """
       Accuracy:
       """
       Accuracy = (self.TruePositives + self.TrueNegatives)/(self.Total)
-      print('Accuracy:', Accuracy)
+      return Accuracy
 
-   def precision(self):
+   def precision(self) -> float:
       """
       Precision:
       """
       Precision = (self.TruePositives)/(self.TruePositives + self.FalsePositives)
-      print('Precision:', Precision)
+      return Precision
 
-   def sensitivity(self):
+   def sensitivity(self) -> float:
       """
       Sensitivity:
       """
       Sensitivity = (self.TruePositives)/(self.FalseNegatives + self.TruePositives)
-      print('Sensitivity:', Sensitivity)
+      return Sensitivity
 
-   def specificity(self):
+   def specificity(self) -> float:
       """
       Specificity:
       """
       Specificity = (self.TrueNegatives)/(self.TrueNegatives + self.FalsePositives)
-      print('Specificity:', Specificity)
+      return Specificity
 
-   def true_positive_rate(self):
+   def true_positive_rate(self) -> float:
       """
       True Positive Rate (TPR):
       """
       TruePositiveRate = (self.TruePositives)/(self.TruePositives + self.FalsePositives)
-      print('True Positive Rate:', TruePositiveRate)
+      return TruePositiveRate
 
-   def false_positive_rate(self):
+   def false_positive_rate(self) -> float:
       """
       False Positive Rate (FPR):
       """
       FalsePositiveRate = (self.FalsePositives)/(self.FalsePositives + self.TrueNegatives)
-      print('False Positive Rate:', FalsePositiveRate)
+      return FalsePositiveRate
 
-   def f1_score(self):
+   def f1_score(self) -> float:
       """
       F1-Score:
       """
       F1Score = (2 * self.accuracy() * self.sensitivity())/(self.accuracy() + self.sensitivity())
-      print('F1-Score:', F1Score)
+      return F1Score
 
    def all_metrics(self):
-      self.accuracy()
-      self.precision()
-      self.sensitivity()
-      self.specificity()
-      self.true_positive_rate()
-      self.false_positive_rate()
-      self.f1_score()
+      print('Accuracy:',self.accuracy())
+      print('Precision:',self.precision())
+      print('Sensitivity:',self.sensitivity())
+      print('Specificity:',self.specificity())
+      print('TPR:',self.true_positive_rate())
+      print('FPR:',self.false_positive_rate())
+      print('F1-Score:',self.f1_score())
