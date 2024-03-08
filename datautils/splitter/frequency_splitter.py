4915b98b0b4848be50fc52e0c1b5d92f6b91dc95
luizantoniona <luizantonio.na@hotmail.com>
2024-03-08 00:56:45 -0300

    refactor: adjust splitters, keep just segmented methods

--- datautils/splitter/frequency_splitter.py
+++ datautils/splitter/frequency_splitter.py
@@ -8,22 +8,14 @@ import model.summary_model as sm
 import datautils.splitter.summary_splitter as splitter
 import numpy as np
 
-def frequency_data_splitter(summaries: list[sm.SummaryModel], split_size=0.2):
-    """
-    Split summaries into training and validation sets based on frequency-domain data.
-    """
-    X_train, X_val, y_train, y_val = splitter.summaries_data_splitter(summaries, split_size)
-
-    X_train_freq_data = np.array([summary.signal.get_freq_data() for summary in X_train])
-    X_val_freq_data = np.array([summary.signal.get_freq_data() for summary in X_val])
-
-    return X_train_freq_data, X_val_freq_data, y_train, y_val
-
 def segmented_frequency_data_splitter(summaries: list[sm.SummaryModel], split_size=0.2):
     """
     Split segmented summaries into training and validation sets based on frequency-domain data.
     """
-    X_train, X_val, y_train, y_val = splitter.segmented_summaries_data_splitter(summaries, split_size)
+    X_train, X_val = splitter.summaries_data_splitter(summaries, split_size)
+
+    y_train = np.concatenate([summary.signal.get_label_segments() for summary in X_train])
+    y_val = np.concatenate([summary.signal.get_label_segments() for summary in X_val])
 
     X_train_segmented_freq_data = np.concatenate([summary.signal.get_freq_segmented_data() for summary in X_train])
     X_val_segmented_freq_data = np.concatenate([summary.signal.get_freq_segmented_data() for summary in X_val])
