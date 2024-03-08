113fc347cf837e27b584ae9e5bae54f54f78f1b5
luizantoniona <luizantonio.na@hotmail.com>
2024-03-08 00:51:45 -0300

    fix: split just the summaries

--- datautils/splitter/summary_splitter.py
+++ datautils/splitter/summary_splitter.py
@@ -5,30 +5,12 @@ This module provides functions to split summary data into training and validatio
 """
 
 import model.summary_model as sm
-import numpy as np
 from sklearn.model_selection import train_test_split
 
-def summaries_data_splitter(summaries: list[sm.SummaryModel], split_size=0.2) -> tuple[list[sm.SummaryModel], list[sm.SummaryModel], np.array, np.array]:
+def summaries_data_splitter(summaries: list[sm.SummaryModel], split_size=0.2) -> tuple[list[sm.SummaryModel], list[sm.SummaryModel]]:
     """
     Split summary data into training and validation sets for model training.
     """
     X_train, X_val = train_test_split(summaries, test_size=split_size, random_state=42)
 
-    y_train = [summary.has_anomaly() for summary in X_train]
-    y_val = [summary.has_anomaly() for summary in X_val]
-
-    y_train = np.array( y_train)
-    y_val = np.array(y_val)
-
-    return X_train, X_val, y_train, y_val
-
-def segmented_summaries_data_splitter(summaries: list[sm.SummaryModel], split_size=0.2) -> tuple[list[sm.SummaryModel], list[sm.SummaryModel], np.array, np.array]:
-    """
-    Split segmented summary data into training and validation sets for model training.
-    """
-    X_train, X_val = train_test_split(summaries, test_size=split_size, random_state=42)
-
-    y_train = np.concatenate([summary.anomalies_by_time_window() for summary in X_train])
-    y_val = np.concatenate([summary.anomalies_by_time_window() for summary in X_val])
-
-    return X_train, X_val, y_train, y_val
+    return X_train, X_val
