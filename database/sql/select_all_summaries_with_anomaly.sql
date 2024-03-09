e4d93813d1db403c9e22f1bdf38a7d410305646e
luizantoniona <luizantonio.na@hotmail.com>
2024-03-09 13:48:23 -0300

    fix: remove problematic files

--- database/sql/select_all_summaries_with_anomaly.sql
+++ database/sql/select_all_summaries_with_anomaly.sql
@@ -1,3 +1,6 @@
 SELECT * FROM summary_info 
     WHERE nr_seizures > 0
-    AND record_name != 'chb12';
+    AND record_name != 'chb24'
+    AND file_name != 'chb12_27.edf'
+    AND file_name != 'chb12_28.edf'
+    AND file_name != 'chb12_29.edf';
