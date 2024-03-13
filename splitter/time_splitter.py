ac329ac8f3b1d1618e19421d6b4e27dae5ffd4a0
luizantoniona <luizantonio.na@hotmail.com>
2024-03-13 01:19:12 -0300

    feat: shufle data to generate randomness

--- splitter/time_splitter.py
+++ splitter/time_splitter.py
@@ -7,6 +7,7 @@ This module provides functions to split summary data into training, validation a
 import model.summary_model as sm
 import splitter.summary_splitter as splitter
 import numpy as np
+from sklearn.utils import shuffle
 
 def test_segmented_time_data(summaries: list[sm.SummaryModel]):
     X_test = summaries
@@ -28,5 +29,7 @@ def train_val_segmented_time_data(summaries: list[sm.SummaryModel], split_size=0
 
     X_train_segmented_time_data = np.concatenate([summary.signal.get_time_segmented_data() for summary in X_train])
     X_val_segmented_time_data = np.concatenate([summary.signal.get_time_segmented_data() for summary in X_val])
+
+    X_train_segmented_time_data, y_train = shuffle(X_train_segmented_time_data, y_train, random_state=0)
 
     return X_train_segmented_time_data, X_val_segmented_time_data, y_train, y_val
