d8330331725a323736a3e23db255a5d59a564367
luizantoniona <luizantonio.na@hotmail.com>
2024-03-13 13:09:40 -0300

    feat: create frequency processor and adjust time

--- preprocessor/frequency_preprocessor.py
+++ preprocessor/frequency_preprocessor.py
@@ -2,8 +2,8 @@
 """
 import random
 import loader.data_loader as loader
-import normalizer.time_normalizer as normalizer
-import splitter.time_splitter as splitter
+import normalizer.frequency_normalizer as normalizer
+import splitter.frequency_splitter as splitter
 import balancer.data_balancer as balancer
 
 def preprocess( balance_train = False, balance_val = False, balance_test = False, number_test_samples = 5 ):
@@ -14,13 +14,13 @@ def preprocess( balance_train = False, balance_val = False, balance_test = False
 
     random.shuffle(summaries)
 
-    loader.load_time_segmented_data(summaries)
+    loader.load_freq_segmented_data(summaries)
 
-    normalizer.normalize_time_data(summaries)
+    normalizer.normalize_frequency_data(summaries)
 
-    X_train, X_val, y_train, y_val = splitter.train_val_segmented_time_data(summaries[:number_train_val_samples])
+    X_train, X_val, y_train, y_val = splitter.train_val_segmented_frequency_data(summaries[:number_train_val_samples])
 
-    X_test, y_test = splitter.test_segmented_time_data(summaries[number_train_val_samples + 1 : number_train_val_samples + 1 + number_test_samples])
+    X_test, y_test = splitter.test_segmented_frequency_data(summaries[number_train_val_samples + 1 : number_train_val_samples + 1 + number_test_samples])
 
     if( balance_train ):
         X_train, y_train = balancer.balance(X_train, y_train)
