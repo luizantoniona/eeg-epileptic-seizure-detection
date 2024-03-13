de6a2d05e2717e5714d9db4f8625dd7fd3c9edb1
luizantoniona <luizantonio.na@hotmail.com>
2024-03-13 01:18:52 -0300

    fix: corrigir método de normalização

--- normalizer/data_normalizer.py
+++ normalizer/data_normalizer.py
@@ -3,11 +3,16 @@ Module: data_normalizer
 
 This module provides functions to normalize data.
 """
-
+from model.summary_model import SummaryModel
 from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
-def normalize(data):
+def normalize_time_data(summaries: list[SummaryModel]):
     """
+    Normalize the time data in each summary.
     """
     scaler = MinMaxScaler()
-    data = scaler.fit_transform(data.reshape(data.shape[0], -1)).reshape(data.shape)
+    for summary in summaries:
+        all_samples = np.concatenate(summary.signal.time_segments)
+        scaler.fit_transform(all_samples)
+        summary.signal.time_segments = [scaler.transform(sample) for sample in summary.signal.time_segments]
