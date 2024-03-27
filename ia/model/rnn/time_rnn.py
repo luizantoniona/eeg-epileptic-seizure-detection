dbdb84db35f4b145e31289fef0a12c1b72fb9c0e
luizantoniona <luizantonio.na@hotmail.com>
2024-03-27 13:22:07 -0300

    feat: adjusting rnn models

--- ia/model/rnn/time_rnn.py
+++ ia/model/rnn/time_rnn.py
@@ -4,20 +4,23 @@ Module: time_rnn
 This module provides RNN models for time domain
 """
 
+from ia.model.rnn.base_rnn import BaseRNN
 import tensorflow as tf
 
-class TimeRNN:
+class TimeRNN( BaseRNN ):
     """
     RNN Model for time data training
     """
     def __init__(self):
-
-        self.model = tf.keras.Sequential()
+        super().__init__()
         self.model.add(tf.keras.layers.Embedding(input_dim=1000, output_dim=64))
         self.model.add(tf.keras.layers.GRU(256, return_sequences=True))
         self.model.add(tf.keras.layers.SimpleRNN(128))
         self.model.add(tf.keras.layers.LSTM(128))
         self.model.add(tf.keras.layers.Dense(10))
+
+    def name(self):
+        return "time_rnn"
 
 
 #SimpleRNN: a fully-connected RNN where the output from previous timestep is to be fed to next timestep
