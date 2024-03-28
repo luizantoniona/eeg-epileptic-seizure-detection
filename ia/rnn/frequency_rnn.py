fa6b29b2f7683a3a545c19ae1f79ab8b67394843
luizantoniona <luizantonio.na@hotmail.com>
2024-03-28 15:58:58 -0300

    feat: clean rnn models and training data

--- ia/rnn/frequency_rnn.py
+++ ia/rnn/frequency_rnn.py
@@ -13,9 +13,8 @@ class FrequencyRNN( BaseRNN ):
     """
     def __init__(self):
         super().__init__()
-        self.model.add(tf.keras.layers.Embedding(input_dim=1000, output_dim=64))
         self.model.add(tf.keras.layers.GRU(256, return_sequences=True))
-        self.model.add(tf.keras.layers.SimpleRNN(128))
+        self.model.add(tf.keras.layers.SimpleRNN(128, return_sequences=True))
         self.model.add(tf.keras.layers.LSTM(128))
         self.model.add(tf.keras.layers.Dense(10))
 
