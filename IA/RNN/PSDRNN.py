9c6603f6dee1834572479aa2f88848dfb3e80b33
luizantoniona <luizantonio.na@hotmail.com>
2024-06-04 14:42:35 -0300

    refactor: correcting rnn layers

--- IA/RNN/PSDRNN.py
+++ IA/RNN/PSDRNN.py
@@ -5,12 +5,18 @@ class PSDRNN( BaseNN ):
     """
     RNN Model for time data training
     """
-    def __init__(self):
+    def __init__(self, input_shape):
         super().__init__()
-        self.model.add(tf.keras.layers.GRU(256, return_sequences=True))
+        self.model.add(tf.keras.layers.GRU(256, return_sequences=True, input_shape=input_shape))
         self.model.add(tf.keras.layers.SimpleRNN(128, return_sequences=True))
         self.model.add(tf.keras.layers.LSTM(128))
-        self.model.add(tf.keras.layers.Dense(1))
+        self.model.add(tf.keras.layers.Dense(128, activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.5))
+        self.model.add(tf.keras.layers.Dense(64, activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.5))
+        self.model.add(tf.keras.layers.Dense(32, activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.5))
+        self.model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
     def name(self):
         """
