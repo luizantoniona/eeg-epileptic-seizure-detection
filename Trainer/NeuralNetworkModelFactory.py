278601c29fa93f74ea4295f8b71a84a2f094efe2
luizantoniona <luizantonio.na@hotmail.com>
2024-06-13 00:20:18 -0300

    feat: Create a neural network factory

---
+++ Trainer/NeuralNetworkModelFactory.py
@@ -0,0 +1,29 @@
+"""
+Module: NeuralNetworkModelFactory
+"""
+from IA.BaseNN import BaseNN
+import IA.CNN.ConvolutionalNNFactory as ConvolutionalNNFactory
+import IA.RNN.RecurrentNNFactory as RecurrentNNFactory
+import IA.CRNN.ConvolutionalRecurrentNNFactory as ConvolutionalRecurrentNNFactory
+import IA.SNN.SiameseNNFactory as SiameseNNFactory
+import IA.ViT.VisionTransformerFactory as VisionTransformerFactory
+
+def model_by_type(model_type: str, signal_type: str, input_shape) -> BaseNN:
+
+    if model_type == "CNN":
+        return ConvolutionalNNFactory.model_by_signal_type(signal_type=signal_type, input_shape=input_shape)
+    
+    elif model_type == "RNN":
+        return RecurrentNNFactory.model_by_signal_type(signal_type=signal_type, input_shape=input_shape)
+
+    elif model_type == "CRNN":  
+        return ConvolutionalRecurrentNNFactory.model_by_signal_type(signal_type=signal_type, input_shape=input_shape)
+    
+    elif model_type == "SNN":
+        return SiameseNNFactory.model_by_signal_type(signal_type=signal_type, input_shape=input_shape)
+
+    elif model_type == "ViT":
+        return VisionTransformerFactory.model_by_signal_type(signal_type=signal_type, input_shape=input_shape)
+
+    else:
+        return BaseNN()
