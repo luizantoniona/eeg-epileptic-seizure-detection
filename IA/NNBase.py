b15408fab7e6b5bd0860b0b9c1d4c6c25f4458ac
luizantoniona <luizantonio.na@hotmail.com>
2024-06-13 18:23:51 -0300

    refactor: Rename class to follow new pattern

--- IA/NNBase.py
+++ IA/NNBase.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
-class BaseNN:
+class NNBase:
     """
     Neural Network Basic Model methods
     """
--- IA/NeuralNetworkModelFactory.py
+++ IA/NeuralNetworkModelFactory.py
@@ -1,14 +1,14 @@
 """
 Module: NeuralNetworkModelFactory
 """
-from IA.BaseNN import BaseNN
+from IA.NNBase import NNBase
 import IA.CNN.ConvolutionalNNFactory as ConvolutionalNNFactory
 import IA.RNN.RecurrentNNFactory as RecurrentNNFactory
 import IA.CRNN.ConvolutionalRecurrentNNFactory as ConvolutionalRecurrentNNFactory
 import IA.SNN.SiameseNNFactory as SiameseNNFactory
 import IA.ViT.VisionTransformerFactory as VisionTransformerFactory
 
-def model_by_type(model_type: str, signal_type: str, input_shape) -> BaseNN:
+def model_by_type(model_type: str, signal_type: str, input_shape) -> NNBase:
 
     if model_type == "CNN":
         return ConvolutionalNNFactory.model_by_signal_type(signal_type=signal_type, input_shape=input_shape)
@@ -26,4 +26,4 @@ def model_by_type(model_type: str, signal_type: str, input_shape) -> BaseNN:
         return VisionTransformerFactory.model_by_signal_type(signal_type=signal_type, input_shape=input_shape)
 
     else:
-        return BaseNN()
+        return NNBase()
