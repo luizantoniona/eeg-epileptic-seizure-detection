27d92ca66264cc07247ac565b94e0f969fa73edd
luizantoniona <luizantonio.na@hotmail.com>
2024-06-29 14:08:17 -0300

    fix: remove SNN

--- IA/NeuralNetworkModelFactory.py
+++ IA/NeuralNetworkModelFactory.py
@@ -5,7 +5,6 @@ from IA.NNBase import NNBase
 import IA.CNN.ConvolutionalNNFactory as ConvolutionalNNFactory
 import IA.RNN.RecurrentNNFactory as RecurrentNNFactory
 import IA.CRNN.ConvolutionalRecurrentNNFactory as ConvolutionalRecurrentNNFactory
-import IA.SNN.SiameseNNFactory as SiameseNNFactory
 import IA.ViT.VisionTransformerFactory as VisionTransformerFactory
 
 def model_by_type(model_type: str, signal_type: str, input_shape) -> NNBase:
@@ -18,9 +17,6 @@ def model_by_type(model_type: str, signal_type: str, input_shape) -> NNBase:
 
     elif model_type == "CRNN":  
         return ConvolutionalRecurrentNNFactory.model_by_signal_type(signal_type=signal_type, input_shape=input_shape)
-    
-    elif model_type == "SNN":
-        return SiameseNNFactory.model_by_signal_type(signal_type=signal_type, input_shape=input_shape)
 
     elif model_type == "ViT":
         return VisionTransformerFactory.model_by_signal_type(signal_type=signal_type, input_shape=input_shape)
