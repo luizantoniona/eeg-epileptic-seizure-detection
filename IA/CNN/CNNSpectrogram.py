6ab1fa5e3fa5dc8aff8eb556dfdf228824da4c5d
luizantoniona <luizantonio.na@hotmail.com>
2024-07-19 23:29:25 -0300

    feat: Adding tuner parameter for CNN models

--- IA/CNN/CNNSpectrogram.py
+++ IA/CNN/CNNSpectrogram.py
@@ -1,5 +1,6 @@
+import keras
+import keras_tuner as kt
 from IA.CNN.CNNBase import CNNBase
-import tensorflow as tf
 
 class CNNSpectrogram( CNNBase ):
     """
@@ -9,19 +10,29 @@ class CNNSpectrogram( CNNBase ):
     def __init__(self, input_shape):
         super().__init__(input_shape)
 
-    def construct_model(self):
-        self.model = tf.keras.models.Sequential()
-        self.model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=self.input_shape, padding='same'))
-        self.model.add(tf.keras.layers.MaxPooling2D((2, 2), padding='same'))
-        self.model.add(tf.keras.layers.BatchNormalization())
-        self.model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
-        self.model.add(tf.keras.layers.MaxPooling2D((2, 2), padding='same'))
-        self.model.add(tf.keras.layers.BatchNormalization())
-        self.model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
-        self.model.add(tf.keras.layers.MaxPooling2D((2, 2), padding='same'))
-        self.model.add(tf.keras.layers.BatchNormalization())
-        self.model.add(tf.keras.layers.GlobalMaxPooling2D())
-        super().create_dense()
+    def construct_model(self, hyper_param: kt.HyperParameters):
+        self.model = keras.models.Sequential()
+        self.model.add(keras.layers.InputLayer(shape=self.input_shape))
+
+        self.model.add(keras.layers.Conv2D(
+            hyper_param.Int(name='filters_1', min_value=8, max_value=128, step=8, default=16),
+            kernel_size=(3, 3),
+            activation='relu')
+        )
+        self.model.add(keras.layers.MaxPooling2D((3, 3)))
+        self.model.add(keras.layers.BatchNormalization())
+
+        self.model.add(keras.layers.Conv2D(
+            hyper_param.Int(name='filters_2', min_value=8, max_value= 128, step=8, default=16),
+            kernel_size=(3, 3),
+            activation='relu')
+        )
+        self.model.add(keras.layers.MaxPooling2D((2, 2)))
+        self.model.add(keras.layers.BatchNormalization())
+
+        self.model.add(keras.layers.Flatten())
+        
+        super().create_dense(hyper_param=hyper_param)
 
     def name(self):
         return super().name()
--- IA/CNN/CNNTime.py
+++ IA/CNN/CNNTime.py
@@ -1,4 +1,5 @@
 import keras
+import keras_tuner as kt
 from IA.CNN.CNNBase import CNNBase
 
 class CNNTime( CNNBase ):
@@ -8,21 +9,29 @@ class CNNTime( CNNBase ):
     def __init__(self, input_shape):
         super().__init__(input_shape)
 
-    def construct_model(self):
+    def construct_model(self, hyper_param: kt.HyperParameters):
         self.model = keras.models.Sequential()
         self.model.add(keras.layers.InputLayer(shape=self.input_shape))
 
-        self.model.add(keras.layers.Conv2D(16, (3, 3), activation='relu'))
+        self.model.add(keras.layers.Conv2D(
+            hyper_param.Int(name='filters_1', min_value=8, max_value=128, step=8, default=16),
+            kernel_size=(3, 3),
+            activation='relu')
+        )
         self.model.add(keras.layers.MaxPooling2D((3, 3)))
         self.model.add(keras.layers.BatchNormalization())
 
-        self.model.add(keras.layers.Conv2D(32, (3, 3), activation='relu'))
+        self.model.add(keras.layers.Conv2D(
+            hyper_param.Int(name='filters_2', min_value=8, max_value= 128, step=8, default=16),
+            kernel_size=(3, 3),
+            activation='relu')
+        )
         self.model.add(keras.layers.MaxPooling2D((3, 3)))
         self.model.add(keras.layers.BatchNormalization())
         
         self.model.add(keras.layers.Flatten())
         
-        super().create_dense()
+        super().create_dense(hyper_param)
 
     def name(self):
         return super().name()
