0afeefd45e2a2d53f4718c2b5607fef45356bb9a
luizantoniona <luizantonio.na@hotmail.com>
2024-07-24 12:59:50 -0300

    refactor: window_length

--- Trainer/Trainer.py
+++ Trainer/Trainer.py
@@ -10,13 +10,13 @@ NR_EPOCHS = 100 #TODO: Use Keras Tuner
 BATCH_SIZE = 256 #TODO: Use Keras Tuner
 
 def train(X_train, y_train, X_val, y_val, X_test, y_test,
-          model_type: str, signal_type: str, time_window: int,
+          model_type: str, signal_type: str, window_length: int,
           learning_rate: float = 0.001):
 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
     def build(hp):
-        neural_network_model = NNModelFactory.model_by_type(model_type, signal_type, X_train[0].shape)
+        neural_network_model = NNModelFactory.model_by_type(model_type, signal_type, X_train[0].shape, window_length)
         neural_network_model.construct_model(hp)
         neural_network_model.compile(learning_rate=learning_rate)
         return neural_network_model.model
@@ -25,13 +25,13 @@ def train(X_train, y_train, X_val, y_val, X_test, y_test,
                                     objective='val_accuracy',
                                     max_trials=20,
                                     directory='Tuner',
-                                    project_name=model_type + "_" + signal_type + "_" + str(time_window))
+                                    project_name=model_type + "_" + signal_type + "_" + str(window_length))
     
     tuner.search_space_summary()
     tuner.search(X_train, y_train, epochs=50, validation_data=(X_val, y_val))
     best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
 
-    neural_network_model = NNModelFactory.model_by_type(model_type, signal_type, X_train[0].shape)
+    neural_network_model = NNModelFactory.model_by_type(model_type, signal_type, X_train[0].shape, window_length)
     neural_network_model.construct_model(best_hps)
 
     print(neural_network_model.name())
@@ -44,7 +44,7 @@ def train(X_train, y_train, X_val, y_val, X_test, y_test,
     neural_network_model.predict(X_test)
     #neural_network_model.print_predictions(y_test)
 
-    metric = Metric(y_test, neural_network_model.predictions, neural_network_model.name(), signal_type, time_window)
+    metric = Metric(y_test, neural_network_model.predictions, neural_network_model.name(), signal_type, window_length)
     metric.all_metrics()
     metric.metrics_to_database()
     # metric.plot_roc_auc(y_test, neural_network_model.predictions)
