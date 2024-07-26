3989724525a76a34fb2e5c9d50fc1b72b2ef56ad
luizantoniona <luizantonio.na@hotmail.com>
2024-07-26 00:14:26 -0300

    refactor: new way to create layers

--- IA/RNN/RNNTime.py
+++ IA/RNN/RNNTime.py
@@ -13,15 +13,13 @@ class RNNTime( RNNBase ):
         self.model = keras.models.Sequential()
         self.model.add(keras.layers.InputLayer(shape=self.input_shape))
         
-        self.model.add(keras.layers.LSTM(
-            hyper_param.Int(name='lstm_units_1', min_value=8, max_value=128, step=8, default=32),
-            return_sequences=True)
-        )
-        self.model.add(keras.layers.LSTM(
-            hyper_param.Int(name='lstm_units_2', min_value=8, max_value=64, step=8, default=16))
-        )
-        self.model.add(keras.layers.Flatten())
+        self.model.add(super().create_lstm_layer(hyper_param, min_value=8, max_value=64, step_value=8, default_value=32, return_sequences=True))
+        self.model.add(super().create_dropout_layer(hyper_param, min_value=0.1, max_value=0.8, step_value=0.1, default_value=0.5))
 
+        self.model.add(super().create_lstm_layer(hyper_param, min_value=8, max_value=32, step_value=8, default_value=16))
+        self.model.add(super().create_dropout_layer(hyper_param, min_value=0.1, max_value=0.8, step_value=0.1, default_value=0.5))
+        
+        self.model.add(keras.layers.Flatten())
         super().create_dense(hyper_param=hyper_param)
 
     def name(self):
