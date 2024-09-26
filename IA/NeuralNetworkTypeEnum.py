9a5387ea15d451c36843060703c5b798313a698e
luizantoniona <luizantonio.na@hotmail.com>
2024-09-25 23:01:06 -0300

    feat: Creating enumeration for Signal and NNModel

---
+++ IA/NeuralNetworkTypeEnum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class NeuralNetworkTypeEnum(Enum):
+    RNN = 1
+    CNN = 2
+    CRNN = 3
---
+++ Object/Signal/SignalTypeEnum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class SignalTypeEnum(Enum):
+    Time = 1
+    PSD = 2
+    Spectrogram = 3
