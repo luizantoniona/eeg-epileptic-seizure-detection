0616e72769f5ca745ab2596080f463b38e3cea53
luizantoniona <luizantonio.na@hotmail.com>
2024-10-01 21:17:23 -0300

    refactor: Refactor neural network widget to comboBox

--- GUI/Component/NeuralNetworkWidget.py
+++ GUI/Component/NeuralNetworkWidget.py
@@ -1,42 +1,47 @@
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QComboBox
 from PyQt5.QtWidgets import QFrame
-from PyQt5.QtWidgets import QHBoxLayout
 from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QRadioButton
 from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QWidget
 
 class NeuralNetworkWidget(QWidget):
+    currentIndexChanged = pyqtSignal()
+
     def __init__(self):
         super().__init__()
+        self.checked: bool = False
         self.network : str = ""
         self.label = QLabel("Network Type:")
 
-        verticalLine = QVBoxLayout()
-        verticalLine.addWidget(self.label)
+        self.separator = QFrame()
+        self.separator.setFrameShape(QFrame.HLine)
+        self.separator.setFrameShadow(QFrame.Sunken)
 
-        horizontalLine = QHBoxLayout()
+        self.combo_box = QComboBox()
+        self.combo_box.currentIndexChanged.connect(self.on_selected)
+        self.combo_box.addItems([
+            "Select Network",
+            "RNN",
+            "CNN",
+            "CRNN",
+        ])
 
-        self.radioRNN = QRadioButton("RNN", self)
-        self.radioCNN = QRadioButton("CNN", self)
-        self.radioCRNN = QRadioButton("CRNN", self)
+        self.custom_layout = QVBoxLayout()
+        self.custom_layout.addWidget(self.separator)
+        self.custom_layout.addWidget(self.label)
+        self.custom_layout.addWidget(self.combo_box)
+        self.setLayout(self.custom_layout)
 
-        self.radioRNN.toggled.connect(self.on_selected)
-        self.radioCNN.toggled.connect(self.on_selected)
-        self.radioCRNN.toggled.connect(self.on_selected)
-
-        horizontalLine.addWidget(self.radioRNN)
-        horizontalLine.addWidget(self.radioCNN)
-        horizontalLine.addWidget(self.radioCRNN)
-
-        line = QFrame()
-        line.setFrameShape(QFrame.HLine)
-        line.setFrameShadow(QFrame.Sunken)
-
-        verticalLine.addLayout(horizontalLine)
-        verticalLine.addWidget(line)
-        self.setLayout(verticalLine)
+    def getChecked(self):
+        return self.checked
 
     def on_selected(self):
-        selected_button = self.sender()
-        if selected_button.isChecked():
-            self.network = selected_button.text()
+        if self.combo_box.currentIndex() > 0 :
+            self.checked = True
+            self.network = self.combo_box.currentText()
+
+        else:
+            self.checked = False
+
+        self.currentIndexChanged.emit()
