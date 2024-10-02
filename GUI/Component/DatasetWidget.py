1ec1c87c37edddddcb8ad6a9aaf205af89ed0f4d
luizantoniona <luizantonio.na@hotmail.com>
2024-10-01 21:16:24 -0300

    feat: Creating component for dataset selection

---
+++ GUI/Component/DatasetWidget.py
@@ -0,0 +1,45 @@
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QComboBox
+from PyQt5.QtWidgets import QFrame
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QWidget
+
+class DatasetWidget(QWidget):
+    currentIndexChanged = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.checked: bool = False
+        self.dataset : str = ""
+        self.label = QLabel("Dataset:")
+
+        self.separator = QFrame()
+        self.separator.setFrameShape(QFrame.HLine)
+        self.separator.setFrameShadow(QFrame.Sunken)
+
+        self.combo_box = QComboBox()
+        self.combo_box.currentIndexChanged.connect(self.on_selected)
+        self.combo_box.addItems([
+            "Select Dataset",
+            "CHBMIT",
+        ])
+
+        self.custom_layout = QVBoxLayout()
+        self.custom_layout.addWidget(self.separator)
+        self.custom_layout.addWidget(self.label)
+        self.custom_layout.addWidget(self.combo_box)
+        self.setLayout(self.custom_layout)
+
+    def getChecked(self):
+        return self.checked
+
+    def on_selected(self):
+        if self.combo_box.currentIndex() > 0 :
+            self.checked = True
+            self.dataset = self.combo_box.currentText()
+
+        else:
+            self.checked = False
+
+        self.currentIndexChanged.emit()
