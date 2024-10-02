73efdf7e72c352f940500769455a9f93a64639f9
luizantoniona <luizantonio.na@hotmail.com>
2024-10-01 21:16:39 -0300

    feat: Creating widget for info show

---
+++ GUI/Component/InfoPanelWidget.py
@@ -0,0 +1,22 @@
+from PyQt5.QtWidgets import QTextEdit
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QWidget
+
+class InfoPanelWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.info_panel = QTextEdit()
+        self.info_panel.setReadOnly(True)
+
+        self.custom_layout = QVBoxLayout()
+        self.custom_layout.addWidget(self.info_panel)
+        self.setLayout(self.custom_layout)
+
+    def write(self, message):
+        self.info_panel.append(message)
+
+    def clear(self):
+        self.info_panel.clear()
+
+    def flush(self):
+        pass
