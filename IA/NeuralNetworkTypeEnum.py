e4b2604e8e3a0970360a5ba85cf11ab07d3c1ce2
luizantoniona <luizantonio.na@hotmail.com>
2024-10-15 13:32:06 -0300

    feat: Mapping none on types

--- IA/NeuralNetworkTypeEnum.py
+++ IA/NeuralNetworkTypeEnum.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class NeuralNetworkTypeEnum(Enum):
+    NONE = 0
     RNN = 1
     CNN = 2
     CRNN = 3
@@ -17,4 +18,5 @@ def neural_network_enum_by_name(neural_network_name: str) -> NeuralNetworkTypeEn
     if neural_network_name in dataset_map:
         return dataset_map[neural_network_name]
 
-    raise ValueError(f"Unknown neural network name: {neural_network_name}")
+    print(f"Unknown neural network name: {neural_network_name}")
+    return NeuralNetworkTypeEnum.NONE
--- Object/Signal/SignalTypeEnum.py
+++ Object/Signal/SignalTypeEnum.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class SignalTypeEnum(Enum):
+    NONE = 0
     Time = 1
     PSD = 2
     Spectrogram = 3
@@ -17,4 +18,5 @@ def signal_enum_by_name(signal_name: str) -> SignalTypeEnum:
     if signal_name in dataset_map:
         return dataset_map[signal_name]
 
-    raise ValueError(f"Unknown signal name: {signal_name}")
+    print(f"Unknown signal name: {signal_name}")
+    return SignalTypeEnum.NONE
