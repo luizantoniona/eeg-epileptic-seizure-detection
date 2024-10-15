43378720db5b587eb10d50c7adf03a18b6eab7eb
luizantoniona <luizantonio.na@hotmail.com>
2024-10-15 00:59:48 -0300

    refacotr: removing jupýter notebooks

--- IA/NeuralNetworkModelFactory.py
+++ IA/NeuralNetworkModelFactory.py
@@ -1,6 +1,7 @@
 """
 Module: NeuralNetworkModelFactory
 """
+
 from Object.Signal.SignalTypeEnum import SignalTypeEnum
 from IA.NNBase import NNBase
 from IA.NeuralNetworkTypeEnum import NeuralNetworkTypeEnum
@@ -8,23 +9,18 @@ import IA.CNN.ConvolutionalNNFactory as ConvolutionalNNFactory
 import IA.RNN.RecurrentNNFactory as RecurrentNNFactory
 import IA.CRNN.ConvolutionalRecurrentNNFactory as ConvolutionalRecurrentNNFactory
 
+
 def model_by_type(model_type: NeuralNetworkTypeEnum, signal_type: SignalTypeEnum, input_shape, window_length: int) -> NNBase:
 
     match model_type:
         case NeuralNetworkTypeEnum.RNN:
-            return RecurrentNNFactory.model_by_signal_type(signal_type=signal_type,
-                                                           input_shape=input_shape,
-                                                           window_length=window_length)
+            return RecurrentNNFactory.model_by_signal_type(signal_type=signal_type, input_shape=input_shape, window_length=window_length)
 
         case NeuralNetworkTypeEnum.CNN:
-            return ConvolutionalNNFactory.model_by_signal_type(signal_type=signal_type,
-                                                               input_shape=input_shape,
-                                                               window_length=window_length)
+            return ConvolutionalNNFactory.model_by_signal_type(signal_type=signal_type, input_shape=input_shape, window_length=window_length)
 
-        case NeuralNetworkTypeEnum.CRNN:  
-            return ConvolutionalRecurrentNNFactory.model_by_signal_type(signal_type=signal_type,
-                                                                        input_shape=input_shape,
-                                                                        window_length=window_length)
+        case NeuralNetworkTypeEnum.CRNN:
+            return ConvolutionalRecurrentNNFactory.model_by_signal_type(signal_type=signal_type, input_shape=input_shape, window_length=window_length)
 
         case _:
             return NNBase()
