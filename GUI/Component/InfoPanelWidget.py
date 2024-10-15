be92a0a65c12c79a308d19061986a5bb08b1ef6b
luizantoniona <luizantonio.na@hotmail.com>
2024-10-15 17:08:10 -0300

    feat: Add author profile component

--- GUI/Component/InfoPanelWidget.py
+++ GUI/Component/InfoPanelWidget.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import Q_ARG
 from PyQt5.QtCore import QMetaObject
 from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QTextEdit
 from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QWidget
@@ -12,8 +13,12 @@ class InfoPanelWidget(QWidget):
         self.info_panel = QTextEdit()
         self.info_panel.setReadOnly(True)
 
+        self.author_label = QLabel('<a href="https://github.com/luizantoniona">Author: Luiz Antonio Nicolau Anghinoni</a>')
+        self.author_label.setOpenExternalLinks(True)
+
         self.custom_layout = QVBoxLayout()
         self.custom_layout.addWidget(self.info_panel)
+        self.custom_layout.addWidget(self.author_label)
         self.setLayout(self.custom_layout)
 
     def write(self, message):
