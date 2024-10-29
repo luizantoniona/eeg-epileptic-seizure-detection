08cd45de189c9abde1122a9d512ebbefc3994c52
luizantoniona <luizantonio.na@hotmail.com>
2024-10-29 20:31:27 -0300

    fix: Treat errors and exceptions

--- Trainer/Trainer.py
+++ Trainer/Trainer.py
@@ -24,22 +24,26 @@ class Trainer:
     @staticmethod
     def train(dataset_type: DatasetTypeEnum, model_type: NeuralNetworkTypeEnum, signal_type: SignalTypeEnum, window_length: int, data, labels):
 
-        best_hps = Trainer.optimize_hyperparameters(dataset_type, model_type, signal_type, window_length, data, labels)
+        try:
+            best_hps = Trainer.optimize_hyperparameters(dataset_type, model_type, signal_type, window_length, data, labels)
 
-        data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size=0.2, random_state=42)
+            data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size=0.2, random_state=42)
 
-        kf = KFold(n_splits=10)
+            kf = KFold(n_splits=10)
 
-        for train_index, val_index in kf.split(data_train):
-            X_train, X_val = data_train[train_index], data_train[val_index]
-            y_train, y_val = labels_train[train_index], labels_train[val_index]
+            for train_index, val_index in kf.split(data_train):
+                X_train, X_val = data_train[train_index], data_train[val_index]
+                y_train, y_val = labels_train[train_index], labels_train[val_index]
 
-            neural_network_model = Trainer.build_and_compile_model(model_type, signal_type, window_length, data_train[0].shape, best_hps)
-            neural_network_model.fit(X_train, y_train, num_epochs=NR_EPOCHS, batch_size=BATCH_SIZE, val_data=X_val, val_labels=y_val)
+                neural_network_model = Trainer.build_and_compile_model(model_type, signal_type, window_length, data_train[0].shape, best_hps)
+                neural_network_model.fit(X_train, y_train, num_epochs=NR_EPOCHS, batch_size=BATCH_SIZE, val_data=X_val, val_labels=y_val)
 
-            neural_network_model.predict(data_test)
-            Trainer.evaluate_metrics(labels_test, dataset_type, model_type, signal_type, window_length, neural_network_model)
-            Trainer.clear_memory()
+                neural_network_model.predict(data_test)
+                Trainer.evaluate_metrics(labels_test, dataset_type, model_type, signal_type, window_length, neural_network_model)
+                Trainer.clear_memory()
+
+        except Exception as e:
+            print(f"FAILED {e}")
 
     @staticmethod
     def optimize_hyperparameters(dataset_type: DatasetTypeEnum, model_type: NeuralNetworkTypeEnum, signal_type: SignalTypeEnum, window_length: int, data, labels):
@@ -62,9 +66,12 @@ class Trainer:
                 tuner.search_space_summary()
                 tuner.search(data_train, labels_train, epochs=50, batch_size=BATCH_SIZE, validation_data=(data_val, labels_val))
                 success = True
-            except:
+
+            except Exception as e:
                 Trainer.clear_memory()
                 attempts += 1
+                if attempts >= max_attempts:
+                    raise e
 
         best_hps = tuner.get_best_hyperparameters()[0]
         return best_hps
