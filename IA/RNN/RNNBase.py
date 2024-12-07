23a66ac2d408b96a811cd261862fb07890923a00
luizantoniona <luizantonio.na@hotmail.com>
2024-12-06 21:44:05 -0300

    feat: Add method for GRU layers

--- IA/RNN/RNNBase.py
+++ IA/RNN/RNNBase.py
@@ -11,6 +11,7 @@ class RNNBase(NNBase):
     def __init__(self, input_shape, window_length: int):
         super().__init__(input_shape, window_length)
         self.lstm_count: int = 0
+        self.gru_count: int = 0
 
     def name(self):
         """
@@ -22,9 +23,20 @@ class RNNBase(NNBase):
         return super().create_dropout_layer(hyper_param, min_value, max_value, step_value, default_value)
 
     def create_lstm_layer(self, hyper_param: kt.HyperParameters, min_value: int, max_value: int, step_value: int, default_value: int, return_sequences: bool = False):
-        lstm_layer = keras.layers.LSTM(hyper_param.Int(name=f"lstm_{self.lstm_count}", min_value=min_value, max_value=max_value, step=step_value, default=default_value), return_sequences=return_sequences)
+        lstm_layer = keras.layers.LSTM(
+            hyper_param.Int(name=f"lstm_{self.lstm_count}", min_value=min_value, max_value=max_value, step=step_value, default=default_value),
+            return_sequences=return_sequences,
+        )
         self.lstm_count = self.lstm_count + 1
         return lstm_layer
+
+    def create_gru_layer(self, hyper_param: kt.HyperParameters, min_value: int, max_value: int, step_value: int, default_value: int, return_sequences: bool = False):
+        gru_layer = keras.layers.GRU(
+            units=hyper_param.Int(name=f"gru_{self.gru_count}", min_value=min_value, max_value=max_value, step=step_value, default=default_value),
+            return_sequences=return_sequences,
+        )
+        self.gru_count += 1
+        return gru_layer
 
     def create_dense(self, hyper_param: kt.HyperParameters):
         self.model.add(super().create_dense_layer(hyper_param, min_value=32, max_value=256, step_value=8, default_value=128))
