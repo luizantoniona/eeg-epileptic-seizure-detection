e35950d04f6812cabf97bc52bfb96e16dce1573c
luizantoniona <luizantonio.na@hotmail.com>
2024-12-07 21:22:14 -0300

    feat: add custom color

--- Utils/Graphics/BarPlotter.py
+++ Utils/Graphics/BarPlotter.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 class BarPlotter:
     """ """
 
-    def __init__(self, models, accuracies, xlabel, title):
+    def __init__(self, models, accuracies, xlabel, title, color="b"):
         self.models = models
         self.accuracies = accuracies
         self.xlabel = xlabel
         self.title = title
+        self.color = color
 
     def plot(self):
         plt.figure(figsize=(12, 8))
-        plt.barh(self.models, self.accuracies, color="b")
+        plt.barh(self.models, self.accuracies, color=self.color)
         plt.xlabel(self.xlabel)
         plt.title(self.title)
         plt.gca().invert_yaxis()
